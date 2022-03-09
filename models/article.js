f6c8e15026c1eb9a6d163a6bb0869fdbab2fe8e2
Miguel Urena <urena_miguel82@outlook.com>
2022-03-08 17:36:39 -0800

    Updated articlesSchema and article.js

---
+++ models/article.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    markdown: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+mondule.exports = mongoose.model('Article', articleSchema)
--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
@@ -6,7 +7,9 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    
+    const article = new Article({
+        
+    })
 })
 
 module.exports = router
