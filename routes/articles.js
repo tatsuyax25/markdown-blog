668be402dd1ebc5fb5c309aca40601163c15ba62
Miguel Urena <urena_miguel82@outlook.com>
2022-03-08 19:57:25 -0800

    Updated articles.js and _form_fields.ejs

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,7 @@ const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.render('articles/new')
+    res.render('articles/new', { article: new Article() })
 })
 
 router.get('/:id', (req, res) => {
