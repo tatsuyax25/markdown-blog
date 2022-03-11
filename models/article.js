0528df747f6a92d46e0ea6b02ff8383699c1e883
Miguel Urena <urena_miguel82@outlook.com>
2022-03-11 10:59:49 -0800

    Added marked and slugify

--- models/article.js
+++ models/article.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const marked = require('marked')
+const slugify = require('slugify')
 
 const articleSchema = new mongoose.Schema({
     title: {
@@ -15,6 +17,17 @@ const articleSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+articleSchema.pre('validate', function() {
+    if (this.title) {
+        this.slug = slugify(this.title, { lower: true, strict: true })
     }
 })
 
