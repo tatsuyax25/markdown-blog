5bb8c2a71b426c35c53a100ff49fab36e453a305
Miguel Urena <urena_miguel82@outlook.com>
2022-03-11 11:47:59 -0800

    Update models/article.js

--- models/article.js
+++ models/article.js
@@ -25,10 +25,12 @@ const articleSchema = new mongoose.Schema({
     }
 })
 
-articleSchema.pre('validate', function() {
+articleSchema.pre('validate', function(next) {
     if (this.title) {
         this.slug = slugify(this.title, { lower: true, strict: true })
     }
+
+    next()
 })
 
 module.exports = mongoose.model('Article', articleSchema)
