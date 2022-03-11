9812e896b917207bcd25dc8ed60af401fb585333
Miguel Urena <urena_miguel82@outlook.com>
2022-03-11 15:06:43 -0800

    Did a method override

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', { 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost/blog', {
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 
 // Get the articles to show on the website.
